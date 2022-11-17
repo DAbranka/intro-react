ddabb742384040ad61db1285d936c8ce37627071
Abranka <davidabranka@gmail.com>
2022-11-17 19:48:51 +0100

    files updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+// todo: Séparer les components dans différent fichiers .js (ex pour le button => button.js).
+// todo: Ecrire en bas du code "export default <fichier.js>".
+// todo: Importer dans App.js les différents components.
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Is it working ?
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <h1> My Todo App</h1>
+      </header>
+      <main>
+      <form>
+          <input type="text" id="firstname" name="fname" placeholder="Type a new todo"></input>
+          <button>Add Todo</button>
+        </form>
+        <hr></hr>
+        <h2>
+          Todos
+        </h2>
+        <ul>
+          <li>Learn React</li>
+          <li>Be Awesome!</li>
+        </ul>
+{/* ------------------------------------------------------------------------- */}
+        {/* <p>
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -15,9 +34,10 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
-      </header>
+          Learn React @ BeCode
+        </a> */}
+{/* ------------------------------------------------------------------------- */}
+      </main>
     </div>
   );
 }
