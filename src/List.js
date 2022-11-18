d399bb06d2251679522df776fc5ac6408e2acde5
Abranka <davidabranka@gmail.com>
2022-11-18 14:37:13 +0100

    List.js updated

--- src/List.js
+++ src/List.js
@@ -1,22 +1,36 @@
-import React from "react";
+/* import React from "react";
 
 export default function List() {
-  const todos = ["My first todo", "My second todo"];
 
+  return (<>
+          <div>
+      <input type={"checkbox"} id="firstLine" name="firstLine"></input>
+      <label for="firstLine">Learn React</label>
+    </div><div>
+        <input type={"checkbox"} id="firstLine" name="firstLine"></input>
+        <label for="firstLine">Be Awesome!</label>
+      </div>
+ </> );
+}
+*/
+
+
+// --------------------------------------------------------------------------------------- //
+
+import React, { useState } from "react";
+
+export default function TodoList() {
+  const initialTodos = ["My first todo", "My second todo"];
+  const [todos, setTodos] = useState(initialTodos);
   return (
-    <>
+    <ul>
       {todos.map((todo) => (
-        <>
-          <div>
-            <input type={"checkbox"} id="firstLine" name="firstLine"></input>
-            <label for="firstLine">Learn React</label>
-          </div>
-          <div>
-            <input type={"checkbox"} id="firstLine" name="firstLine"></input>
-            <label for="firstLine">Be Awesome!</label>
-          </div>
-        </>
+        <li>
+          <input type="checkbox" /> {todo}
+        </li>
       ))}
-    </>
+    </ul>
   );
 }
+
+console.log(TodoList)
